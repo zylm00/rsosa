f3a0808d433fa7accbcc43d860ab8b44040ee1af
zylm00 <1123317122@qq.com>
2025-11-28 17:10:49 +0800

    Update desktop_home_page.dart

--- flutter/lib/desktop/pages/desktop_home_page.dart
+++ flutter/lib/desktop/pages/desktop_home_page.dart
@@ -353,7 +353,7 @@ class _DesktopHomePageState extends State<DesktopHomePage>
             ],
           ),
           SizedBox(
-            height: 10.0,
+            height: 12.0,
           ),
           if (!isOutgoingOnly)
             Text(
